fbb041ea5b4c85560ba3e26403ba9362a6150f99
sainasujeer <125561903+sainasujeer@users.noreply.github.com>
2023-02-16 17:57:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ streamlit.text('Coffee,Tea,Porotta')
 streamlit.text('Paneer, Dosa, Chutney')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
