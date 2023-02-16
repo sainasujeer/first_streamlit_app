30e80ad7366d5235626dd109ca54ed8a4ad305be
sainasujeer <125561903+sainasujeer@users.noreply.github.com>
2023-02-16 17:57:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Coffee,Tea,Porotta')
 streamlit.text('Paneer, Dosa, Chutney')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
