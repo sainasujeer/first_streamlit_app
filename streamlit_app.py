705c3ac4feb92a69b46061255a9eae42400ffb7d
sainasujeer <125561903+sainasujeer@users.noreply.github.com>
2023-02-17 14:58:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflke.connector
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My parents new healthy diner')
